547bb4d2f359aae59705c26676177e01be3d07d9
TeuzimMG <basemagalha@gmail.com>
2023-12-23 07:22:24 -0300

    alterações

--- front-end/projeto/src/App.jsx
+++ front-end/projeto/src/App.jsx
@@ -2,13 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './pages/Home'
-import './App.css'
 
 function App() {
   return(
-  
-  <Home/>
-  
+    <Home/>
   )
 }
 
--- front-end/projeto/src/pages/Home.jsx
+++ front-end/projeto/src/pages/Home.jsx
@@ -4,25 +4,26 @@ import Image from '../assets/image.png'
 function Home() {
   return (
     <div className="login">
-			<section className="esquerda">
-				<div className="box">
-					<div className="box-imagem">
-						<img src={Image}/>
-					</div>
-					<div className="box-textos">
-						<h1>LOGIN DA PLATAFORMA</h1>
-						<span>SISTEMA-ESCOLAR</span>
-					</div>
+		<section className="esquerda">
+			<div className="box">
+				<div className="box-imagem">
+					<img src={Image}/>
 				</div>
-			</section>
-			<section className="direita">
-				<div className="titulos">
+				<div className="box-textos">
 					<h1>LOGIN DA PLATAFORMA</h1>
 					<span>SISTEMA-ESCOLAR</span>
 				</div>
-				<div className="content">
-				</div>
-			</section>
+			</div>
+		</section>
+		<section className="direita">
+			<div className="titulos">
+				<h1>LOGIN DA PLATAFORMA</h1>
+				<span>SISTEMA-ESCOLAR</span>
+			</div>
+			<div className="content">
+				<p>TEste</p>
+			</div>
+		</section>
     </div>
   );
 }
